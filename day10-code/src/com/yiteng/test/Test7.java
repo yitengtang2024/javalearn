8021f8898b908f390c19d4a0beead21dd72a06b6
yitengtang2024 <yiteng.tang.yan@gmail.com>
2024-11-30 09:08:59 -0500

    save change

--- day10-code/src/com/yiteng/test/Test7.java
+++ day10-code/src/com/yiteng/test/Test7.java
@@ -2,23 +2,24 @@ package com.yiteng.test;
 
 public class Test7 {
     public static void main(String[] args) {
-        String s = "   Hello world    ";
+        String s = "   Hello world";
         System.out.println(s);
-        String lastWordR = getWord(s);
-        String lastWord = getWord(lastWordR);
+
+        String lastWord = getWord(s);
         System.out.println(lastWord);
         System.out.println(lastWord.length());
 
     }
     private static String getWord(String str){
         StringBuilder sb = new StringBuilder();
-        while(true){
-            for (int i = str.length()-1; i >=0; i--) {
-                sb.append(str.charAt(i));
-                if(str.charAt(i) != ' '&& str.charAt(i-1) == ' ' ){
-                    return sb.toString();
-                }
-            }
+        int i = str.length()-1;
+        while(i>=0 && str.charAt(i)==' '){
+            i--;
         }
+        while(i>=0 && str.charAt(i)!=' '){
+            sb.append(str.charAt(i));
+            i--;
+        }
+        return sb.toString();
     }
 }
